b111cbad36e28830bc291d0a79a3c0ec284eb54c
shtamtails <maximyudin1@gmail.com>
2023-02-02 00:11:37 +0300

    206-reverse-linked-list

--- 206. Reverse Linked List/solution.ts
+++ 206. Reverse Linked List/solution.ts
@@ -26,14 +26,14 @@ The number of nodes in the list is the range [0, 5000].
 Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 */
 
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
+// class ListNode {
+//   val: number;
+//   next: ListNode | null;
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
 
 function reverseList(head: ListNode | null): ListNode | null {
   let next = null;
@@ -48,7 +48,3 @@ function reverseList(head: ListNode | null): ListNode | null {
   }
   return prev;
 }
-
-const list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
-
-console.log(reverseList(list));
