cffa67640a3f6aec20dccf88ad4c897b28ac0024
shtamtails <maximyudin1@gmail.com>
2023-02-02 17:53:28 +0300

    876-middle-of-the-linked-list

--- 876. Middle of the Linked List/solution.ts
+++ 876. Middle of the Linked List/solution.ts
@@ -1,11 +1,11 @@
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
+// class ListNode {
+//   val: number;
+//   next: ListNode | null;
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
 
 function middleNode(head: ListNode | null): ListNode | null {
   const getListLength = (list: ListNode) => {
@@ -24,7 +24,3 @@ function middleNode(head: ListNode | null): ListNode | null {
   }
   return head;
 }
-
-const list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
-
-console.log(middleNode(list));
